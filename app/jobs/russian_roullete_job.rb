41e74860e55bd4e172aa0ae4ac90cc0128a5676c
Ryszard Gurzeda <ryszard.gurzeda@netguru.co>
2019-09-21 20:41:44 +0200

    Add sidekiq_options to Job, add temporary comments

--- app/jobs/russian_roullete_job.rb
+++ app/jobs/russian_roullete_job.rb
@@ -1,8 +1,10 @@
 class RussianRoulleteJob < ApplicationJob
   queue_as :default
+  sidekiq_options retry: 10, backtrace: 20
 
   def perform(*args)
-    raise 'You are dead' if rand(1..6) == 1
+    # raise 'You are dead'
+    # raise 'You are dead' if rand(1..6) == 1
 
     logger.info('You are alive!! WO-HOO')
   end
